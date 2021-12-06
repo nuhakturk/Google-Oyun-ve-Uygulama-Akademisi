d178feebf5ce196e75b49d5933074aad5998b537
Nuh Aktürk <nuhar42@gmail.com>
2021-12-06 19:26:05 +0300

    ornekler yapildi

---
+++ StringConcatanate/StringConcatanate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConcatanate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string birinci = "Nuh";
+            string ikinci = "Oyun ve Uygulama Akademisi katılımcısı " + birinci;
+            string ucuncu = "Oyun ve Uygulama Akademisi katılımcısı " + birinci + " " + "Aktürk";
+            Console.WriteLine(ucuncu);
+            Console.ReadLine();
+        }
+    }
+}
