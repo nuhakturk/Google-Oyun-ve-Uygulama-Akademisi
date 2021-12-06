bbbdedcd7dbe4ea61970c5e410214f71f9bba010
Nuh Aktürk <nuhar42@gmail.com>
2021-12-06 15:22:56 +0300

    first commit

---
+++ Deklerasyon/Deklerasyon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Deklerasyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.ReadLine();
+        }
+    }
+}
