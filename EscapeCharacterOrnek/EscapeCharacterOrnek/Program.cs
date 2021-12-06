5a100702c18de37e4d46070bdbf1ed23a3fd98e2
Nuh Aktürk <nuhar42@gmail.com>
2021-12-06 19:13:58 +0300

    Ornekler yapildi

---
+++ EscapeCharacterOrnek/EscapeCharacterOrnek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeCharacterOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello\nWorld");
+            Console.WriteLine("Hello\tWorld");
+            Console.WriteLine("\"Hello World\""); // "Hello World"
+            Console.WriteLine("c:\\users\\nuh\\appdata"); // c:\users\tolgay\appdata - \n \t \" \' \\ @ \u1234
+            Console.WriteLine(@"c:\users\nuh\appdata"); // c:\users\tolgay\appdata  - @ işareti koyduktan sonra backward slash işareti olmadan string yazabiliyorsunuz.
+            Console.ReadLine();
+        }
+    }
+}
