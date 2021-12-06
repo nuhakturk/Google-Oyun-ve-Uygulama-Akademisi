be062ea727c63f4c17fa196af54490cafbb0b10e
Nuh Aktürk <nuhar42@gmail.com>
2021-12-06 19:01:41 +0300

    Ornekler yapildi

---
+++ ImplicitDeclarationOrnek/ImplicitDeclarationOrnek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitDeclarationOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İzmir'de bomba yedim ve tanesi 5 liraydı ve hava sıcaklığı 25,5 dereceydi.
+            string sehir = "Ankara";
+            string yiyecek = "lahmacun";
+            int fiyat = 10;
+            float havaSicakligi = 27.4f;
+            Console.WriteLine("{0}'da {1} yedim ve tanesi {2} liraydı ve hava sıcaklığı {3} dereceydi",sehir, yiyecek, fiyat, havaSicakligi);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ImplicitDeclarationOrnek/ImplicitDeclarationOrnek/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImplicitDeclarationOrnek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImplicitDeclarationOrnek")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f43a1c0-d4d2-4b87-a2fd-0078d521c42e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
