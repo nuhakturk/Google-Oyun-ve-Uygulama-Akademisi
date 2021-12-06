cdaa780c5627e956192cc20a1f09bad10536dcf8
Nuh Aktürk <nuhar42@gmail.com>
2021-12-06 15:11:22 +0300

    Add project files.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static int a; //private - başka bir classtan erişilemez.
+        static void Main(string[] args)
+        {
+            a = 1; //2 3 4 100
+            Console.WriteLine(123);
+            Console.WriteLine('B');
+            //Console.WriteLine("Hello World!"); //Burası bir yorum, Bu fonksiyon console da Hello World Yazılmasını sağlar.
+            //Console.WriteLine("Bu bir satırdır.");
+            //Console.WriteLine("Bu da bir satırdır.");
+            Console.ReadLine();
+        }
+    }
+}
