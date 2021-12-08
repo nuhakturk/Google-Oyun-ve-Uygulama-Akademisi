60b7069608cdcc25cd8c5d2c50f34e5c4cda9256
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 05:11:41 +0300

    array ornekleri yapildi

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] siparisNo = new string[3]; //memory de array boyutu kadar yer açılır.
+
+            siparisNo[0] = "INZ123";
+            siparisNo[1] = "GOOGLE2";
+            siparisNo[2] = "GOOGLE3";
+
+            siparisNo[0] = "INZ345"; //değişiklik
+            */
+
+            string[] siparisNo = {"birinci siparis", "ikinci siparis", "ucuncu siparis"};
+
+            Console.WriteLine(siparisNo[2]);
+            Console.WriteLine("Dizi Uzunluğu: " + siparisNo.Length);
+            Console.ReadLine();
+        }
+    }
+}
