8f7b41d9c8517daef94121c73e397d5ae7803890
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 05:40:57 +0300

    ornekler yapildi

---
+++ LocalveGlobal/LocalveGlobal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalveGlobal
+{
+    class Program
+    {
+        //public static int sayi2;
+        static void Main(string[] args)
+        {
+            int sayi2 = 0;
+            int sayi = 5;
+            if (sayi == 5)
+            {
+                sayi2 = 20;
+            }
+
+            Console.WriteLine(sayi2);
+            Console.ReadLine();
+        }
+    }
+}
