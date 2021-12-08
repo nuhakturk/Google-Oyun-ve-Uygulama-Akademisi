2056b97a24d46ba1f3649c87e141441a46ad71b8
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 04:18:21 +0300

    Math class ornekleri yapildi

--- YeniClassOlusturma/YeniClassOlusturma/Program.cs
+++ YeniClassOlusturma/YeniClassOlusturma/Program.cs
@@ -14,8 +14,23 @@ namespace YeniClassOlusturma
 
             inst.birinci = 10;
             inst.ikinci = 20;
+
+
             Console.WriteLine(inst.birinci + inst.ikinci);
             Console.WriteLine(inst.toplama());
+
+            //10 un 2. kuvveti
+            //en küçük değer
+            //mutlak değer
+
+            float bir = 10f;
+            float iki = 2f;
+
+            Console.WriteLine(Math.Pow(bir, iki)); //100
+            Console.WriteLine(Math.Min(20, 10));  //10
+            Console.WriteLine(Math.Abs(-25.4f));  //25.4
+
+
 
             Console.ReadLine();
         }
