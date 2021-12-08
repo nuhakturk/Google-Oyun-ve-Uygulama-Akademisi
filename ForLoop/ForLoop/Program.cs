d26873daa36952cec61116ef38060999fc55851c
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 21:34:50 +0300

    ornekler yapildi

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 10; i > 0; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            string[] siparisNo =
+            {
+                "INZ001",
+                "GGLE01",
+                "APP01"
+            };
+
+            for(int i = siparisNo.Length; i > 0; i--)
+            {
+                Console.WriteLine(siparisNo[i - 1]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
