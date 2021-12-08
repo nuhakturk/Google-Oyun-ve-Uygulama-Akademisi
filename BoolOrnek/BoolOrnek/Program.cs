07671537537f95efbc9986ed1e0e9c18207de3f4
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 05:27:58 +0300

    bool ornekleri yapildi

---
+++ BoolOrnek/BoolOrnek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 5;
+            string isim = "Nuh Aktürk";
+            Console.WriteLine("a" == "a"); //2 tane string karşılaştırma | Çıktısı= True
+            Console.WriteLine("b" == "B"); // False
+            Console.WriteLine(1.2f == 1.3f); // False
+            Console.WriteLine(1 == sayi); // False
+            Console.WriteLine(1 != sayi); // True
+            Console.WriteLine(isim.Contains("Nu")); // True
+            Console.ReadLine();
+        }
+    }
+}
