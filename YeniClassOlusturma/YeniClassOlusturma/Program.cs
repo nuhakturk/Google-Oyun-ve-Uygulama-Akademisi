57c8400e5b14882fd9961cac034544612ce9a1f1
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 04:02:26 +0300

    ornekler yapildi

---
+++ YeniClassOlusturma/YeniClassOlusturma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeniClassOlusturma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Yeni inst = new Yeni();
+
+            inst.birinci = 10;
+            inst.ikinci = 20;
+
+            Console.WriteLine(inst.birinci + inst.ikinci);
+            Console.ReadLine();
+        }
+
+        class Yeni
+        {
+            public int birinci;
+            public int ikinci;
+        }
+    }
+}
