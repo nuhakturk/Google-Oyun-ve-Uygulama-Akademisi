e8163ef73e5d92eb528f5420e285230a1360378b
Nuh Aktürk <nuhar42@gmail.com>
2021-12-08 21:47:29 +0300

    ornekler yapildi

---
+++ DoWhile_While/DoWhile_While/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            //do
+            //{
+            //    sayi++;
+            //    if (sayi == 7)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(sayi);
+            //} while (sayi < 10);
+
+            while (sayi < 10)
+            {
+                sayi++;
+                if (sayi == 7)
+                {
+                    continue;
+                }
+                Console.WriteLine(sayi);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
